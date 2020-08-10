fa4c81c06682f93b2e3d4b1bd9c894e67f80d762
Anthony Polito <anthonyalexanderpolito@gmail.com>
2020-08-10 01:56:06 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,10 +2,9 @@ until levitation_force = 10
   levitation_force = 6
   puts "The current levitation_force is less than 10."
   levitation_force += 1
-
 end
 
-until counter == 20
-  puts "The current number is less than 20."
-  counter += 1
-end
+# until counter == 20
+#   puts "The current number is less than 20."
+#   counter += 1
+# end
